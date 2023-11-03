db36dcdaf498c84145d39be490dc2d6f406afcef
Yoana Ivanova <135585128+yhi96@users.noreply.github.com>
2023-11-03 09:34:54 +0200

    Add files via upload

---
+++ 05.Journey/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string holidayType = "";
+            double cost = 0;
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    cost = budget * 0.3;
+                    holidayType = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    cost = budget * 0.7;
+                    holidayType = "Hotel";
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    cost = budget * 0.4;
+                    holidayType = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    cost = budget * 0.8;
+                    holidayType = "Hotel";
+                }
+            }
+            else if (budget > 1000)
+            {
+                destination = "Europe";
+                cost = budget * 0.9;
+                holidayType = "Hotel";
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{holidayType} - {cost:F2}");
+        }
+    }
+}
